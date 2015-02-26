29b45e8a0cbe58afcdb308ab2c9ccad45da28353
sakon <sakon.mybp@gmail.com>
2015-02-26 18:35:57 +0700

    --

--- src/main/java/com/mybp/cus/mrperfume/bean/ItemLookUpManagedBean.java
+++ src/main/java/com/mybp/cus/mrperfume/bean/ItemLookUpManagedBean.java
@@ -1,7 +1,14 @@
 package com.mybp.cus.mrperfume.bean;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
+
+import com.mybp.cus.mrperfume.dto.ItemDTO;
+import com.mybp.cus.mrperfume.dto.PaggingWrapper;
+import com.mybp.cus.mrperfume.service.DealerPoService;
 /**
  * @author MrMai
  *
@@ -10,7 +17,71 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class ItemLookUpManagedBean {
 
+	@ManagedProperty(value="#{dealerPoService}")
+	private DealerPoService dealerPoService;
+	
+	private ItemDTO criteria = new ItemDTO();
+	
+	PaggingWrapper<ItemDTO> searchItem = new PaggingWrapper<ItemDTO>();
+	
+	int currentPage = 1;
+	
+	int rowPerPage = 10;
 
+	@PostConstruct
+	public void init(){
+		
+	}
+	
+	public void search(){
+		this.searchItem = dealerPoService.searchItem(criteria, currentPage, rowPerPage);
+	}
+	
+	public void search(ActionEvent actionEvent){
+		this.search();
+	}
+	
+	public void reset(){
+		
+	}
+	
+	public void reset(ActionEvent actionEvent){
+		this.reset();
+	}
+	
+	public DealerPoService getDealerPoService() {
+		return dealerPoService;
+	}
+
+	public void setDealerPoService(DealerPoService dealerPoService) {
+		this.dealerPoService = dealerPoService;
+	}
+
+	public PaggingWrapper<ItemDTO> getSearchItem() {
+		return searchItem;
+	}
+
+	public void setSearchItem(PaggingWrapper<ItemDTO> searchItem) {
+		this.searchItem = searchItem;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public int getRowPerPage() {
+		return rowPerPage;
+	}
+
+	public void setRowPerPage(int rowPerPage) {
+		this.rowPerPage = rowPerPage;
+	}
+	
+	
 	
 	
 }
